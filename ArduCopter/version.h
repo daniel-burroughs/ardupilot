09db2b1ed4c200497a6e197a984080d5bb3b82fe
= <dan@hoverflytech.com>
2020-11-11 09:44:49 -0500

    Saving existing version in new branch

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.3-HF07"
+#define THISFIRMWARE "ArduCopter V4.0.3-HF08"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
